d0fe47ff0eb705cf4147b8a10b511352519ee23a
BrydeRov <andresjavieroksi@gmail.com>
2022-06-12 04:31:50 -0500

    sizes select in progress

--- main.js
+++ main.js
@@ -6,7 +6,15 @@ const app = Vue.createApp({
             image: './assets/images/socks_green.jpg',
             url: 'https:/github.com',
             inventory: 10,
-            onSale: true
+            onSale: true,
+            details: ['50% Cotton' , '%30 Wool', '20% Polyester'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' }
+            ],
+            sizes: [
+                {id: 1, size:'S'}
+            ],
         }
     }
 })
