fc696f007f774cd230b9606b4ee8e0c129d66ba1
BrydeRov <andresjavieroksi@gmail.com>
2022-06-13 01:58:38 -0500

    Lesson 7

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
 const app = Vue.createApp({
     data() {
         return{
+            cart: 0,
             product: 'Socks',
             description: 'This are some beautiful socks!',
+            logo: './assets/images/moda.png',
             image: './assets/images/socks_green.jpg',
             url: 'https:/github.com',
-            inventory: 10,
+            inventory: 100,
             onSale: true,
             details: ['50% Cotton' , '%30 Wool', '20% Polyester'],
             variants: [
-                { id: 2234, color: 'green' },
-                { id: 2235, color: 'blue' }
+                { id: 2234, color: '#35dc88', image: './assets/images/socks_green.jpg' },
+                { id: 2235, color: '#5a7598', image: './assets/images/socks_blue.jpg' }
             ],
             sizes: [
                 {id: 1, size:'S'}
             ],
+        }
+    },
+    methods: {
+        addToCart(){
+            this.cart += 1
+        },
+        removeFromCart(){
+            this.cart -= 1
+        },
+        updateImage(variantImage){
+            this.image = variantImage
         }
     }
 })
