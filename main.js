e01b44120b51578843c1f9ed4a2191625d16dca7
BrydeRov <andresjavieroksi@gmail.com>
2022-06-14 17:17:24 -0500

    Ended L8 without coding challenge

--- main.js
+++ main.js
@@ -2,20 +2,16 @@ const app = Vue.createApp({
     data() {
         return{
             cart: 0,
+            brand: 'Vue Mastery',
+            website_title: 'VUEJS CRUD',
             product: 'Socks',
             description: 'This are some beautiful socks!',
             logo: './assets/images/moda.png',
-            image: './assets/images/socks_green.jpg',
-            url: 'https:/github.com',
-            inventory: 100,
-            onSale: true,
+            selectedVariant: 0,
             details: ['50% Cotton' , '%30 Wool', '20% Polyester'],
             variants: [
-                { id: 2234, color: '#35dc88', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: '#5a7598', image: './assets/images/socks_blue.jpg' }
-            ],
-            sizes: [
-                {id: 1, size:'S'}
+                { id: 2234, color: '#35dc88', image: './assets/images/socks_green.jpg', quantity: 0, price: 70 },
+                { id: 2235, color: '#5a7598', image: './assets/images/socks_blue.jpg', quantity: 50, price: 75 }
             ],
         }
     },
@@ -26,8 +22,22 @@ const app = Vue.createApp({
         removeFromCart(){
             this.cart -= 1
         },
-        updateImage(variantImage){
-            this.image = variantImage
+        updateVariant(index){
+            this.selectedVariant = index
+        }
+    },
+    computed: {
+        title(){
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        price() {
+            return this.variants[this.selectedVariant].price
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
         }
     }
 })
